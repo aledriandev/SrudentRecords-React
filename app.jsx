976b37a31ac17049dc95b9c78bdefa1a99ab12c9
AQP20171679 <alejandraadriantejada@gmail.com>
2017-09-30 08:23:42 -0500

    muestra

--- app.jsx
+++ app.jsx
@@ -8,65 +8,48 @@ function Student(name, teachPoints, lifePoints) {
 class StudentsRecord extends React.Component {
     constructor(props) {
         super(props);
-
+        this.estudiantes = [];
         this.state = {
             title: props.title,
-            students: []
+            students: [],
+            student: null
         };
     }
 
     addStudent (e) {
-        console.log('hice click!');
         let name = prompt("Nombre de la estudiante").toUpperCase();
         let teachPoints = parseInt(prompt("Porcentaje Técnico"));
         let lifePoints = parseInt(prompt("Porcentaje Habilidades Socio-Emocionales"));
         let student = new Student(name, teachPoints, lifePoints);
         this.setState({
-            students: this.state.students.concat([student])
+            student: student,
+            students:[]
         });
-        console.log(this.state.students);
-
-        console.log(this.state.students.length);
-        console.log(student);
+        this.estudiantes.push(student);
+        console.log(this.estudiantes);
     }
-    // onSubmit(e) {
-    //     e.preventDefault();
-    //     console.log("onSubmit");
-    //     var item = {
-    //         text: this.state.text,
-    //         checked: false
-    //     };
-    //     this.setState({
-    //         text: "",
-    //         todolist: this.state.todolist.concat([item])
-    //     });
-    // }
-    // onChange(e) {
-    //     console.log("onChange", e.target.value);
-    //     this.setState({
-    //         text: e.target.value,
-    //         todolist: this.state.todolist
-    //     });
-    // }
-    showStudent (student,index) {
-
-        return (
-            <div key={index}>
-                <p>Nombre: {student.name}</p>
-                <p>Puntaje Tecnico: {student.teachPoints}</p>
-                <p>Puntaje HSE: {student.lifePoints}</p>
-                <p>Estatus: {student.status}</p>
-            </div>
-        );
+    showStudent () {
+        if(this.state.student){
+            return (
+                <div >
+                    <p>Nombre: {this.state.student.name}</p>
+                    <p>Puntaje Tecnico: {this.state.student.teachPoints}</p>
+                    <p>Puntaje HSE: {this.state.student.lifePoints}</p>
+                    <p>Estatus: {this.state.student.status}</p>
+                </div>
+            );
+        }
+        else
+            return null;
     }
-
-    printAll (e) {
-        console.log('yai')
-        return true;
+    printAll(){
+        this.setState({
+            student:null,
+            students:this.estudiantes
+        });
     }
-
-    showStudents (students) {
-        return students.map((student,index)=>{
+    showStudents () {
+        return this.state.students.map((student,index)=>{
             return (
                 <div key={index}>
                     <p>Nombre: {student.name}</p>
@@ -77,18 +60,13 @@ class StudentsRecord extends React.Component {
             );
         });
     }
-
+    updateDropout () {
+        console.log(this.estudiantes);
+    }
+    ordenar () {
+        console.log('holi')
+    }
     render() {
-        let student;
-        if (this.state.students.length > 0){
-            let index = (this.state.students).length-1;
-            student = this.showStudent(this.state.students[index],index);
-        }
-        let students;
-        if(this.printAll()){
-            students = this.showStudents(this.state.students);
-        }
-
         console.log(this.state.students.length);
         return (
             <div>
@@ -96,11 +74,11 @@ class StudentsRecord extends React.Component {
                     <div className="nav-wrapper">
                         <h1>{this.state.title}</h1>
                     </div>
-                </header>
+                </header    >
                 <section className="container">
                     <div className="row">
                         <div className="col-md-3">
-                            <button onClick={e => this.addStudent(e)} className="btn btn-success">Add Student</button>
+                        <button onClick={e => this.addStudent(e)} className="btn btn-success">Add Student</button>
                         </div>
                         <div className="col-md-3">
                             <button onClick={e=> this.printAll(e)} className="btn btn-success">Print All</button>
@@ -113,13 +91,10 @@ class StudentsRecord extends React.Component {
                         </div>
                     </div>
                 </section>
-                <section>
-                    {student &&
-                        <div>{student}</div>
-                    }
-                    {this.printAll() &&
-                        <div>{students}</div>
-                    }
+                <section>   
+                    <div>{this.showStudent()}</div>
+                    <div>{this.showStudents()}</div>
+                    <div>{this.ordenar()}</div>
                 </section>
             </div>
         );
